ca21373c3160ae2b1e20897b53d8fdf64db0c6b5
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-11-10 22:57:16 -0600

    pushed code

---
+++ app/src/main/java/com/example/chatapp/ImgurAttachment.java
@@ -0,0 +1,14 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImgurAttachment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attachment_imgur);
+    }
+}
